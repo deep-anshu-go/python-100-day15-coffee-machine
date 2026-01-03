805b9aeb83471839c18c21d79af47d209b0cce6a
deep-anshu-go <anshudeep077@gmail.com>
2026-01-03 13:23:45 +0530

    Refactor coffee machine code to import MENU directly and enhance ingredient handling functions

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 import art 
-import menu
+from menu import MENU
 
 resources = {
     "water": 300,
@@ -42,17 +42,60 @@ def checkSecretCode():
     if(secret_code == "offcoff"):
         return False
     else:
-        askMenu(menu.MENU, msg = "**Inavlid Secret**")
+        askMenu(MENU, msg = "**Inavlid Secret**")
+
+def get_total_ingredients_arr():
+    max_ingredient_length = 0
+    total_ingredients = {} 
+    for key in MENU:
+        ingredient_length = len(MENU[key]["ingredients"])
+        if(ingredient_length > max_ingredient_length):
+            max_ingredient_length = ingredient_length
+            total_ingredients = MENU[key]["ingredients"]
+    
+    ingredients = []
+    for key in total_ingredients:
+        ingredients.append(key)
+    
+    return ingredients
+    
+
+def check_ingredients(coffee_name):
+    
+    ingredients = get_total_ingredients_arr()
+    coffee_ingredients = MENU[coffee_name]["ingredients"]
+    for ingredient in ingredients:
+        try:
+            MENU[coffee_name]["ingredients"][ingredient]
+        except KeyError:
+            coffee_ingredients.update({ingredient: 0})
+            
+    
+    return coffee_ingredients
+
+def check_coffee_resources(coffee_name):
+    check_ingredients(coffee_name)
+    # you are here you got the coffee ingredients 
+
+        
+
+def check_coffee(user_input):
+    if user_input in MENU:
+        check_coffee_resources(coffee_name = user_input)
+    else:
+        return "Invalid coffee name"
 
 def coffeeMachine():
+   
     on = True
     while(on):
-        user_input = askMenu(menu.MENU)
+        user_input = askMenu(MENU)
         if(user_input == "off"):
            on = checkSecretCode()
         elif(user_input == "report"):
             # here, I pass the machine function which is coffeeMachine() to the report function to restart the machine after looking at the report
             report(machine = coffeeMachine)
             on = False
-        
+        else:
+            check_coffee(user_input)
 coffeeMachine()
