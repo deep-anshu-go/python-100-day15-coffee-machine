a9c13622fe5535b930591e38c2a851ecd7551e3e
deep-anshu-go <anshudeep077@gmail.com>
2026-01-06 15:04:11 +0530

    Enhance coffee machine functionality by improving coin handling and resource management

--- index.py
+++ index.py
@@ -7,6 +7,13 @@ resources = {
     "coffee": 100,
 }
 
+coins = {
+    "quarters": 0.25,
+    "dimes": 0.10,
+    "nickles": 0.05,
+    "pennies": 0.01
+}
+
 money = 0
 
 def formatResources(resources):
@@ -45,7 +52,6 @@ def checkSecretCode():
         askMenu(MENU, msg = "**Inavlid Secret**")
 
 def fill_missing_ingredients():
-    resources_ingredients_length = len(resources)
     for coffee in MENU:
         for ingredients in resources:
             if(not ingredients in MENU[coffee]["ingredients"]):
@@ -60,36 +66,85 @@ def is_resources_available(coffee_resources, user_input_coffee_name):
             ingredient = ingredients
     return available, ingredient
 
-def is_enough_resource_available(coffee_resources, coffee_name):
+def get_coffee_price(coffee):
+    return MENU[coffee]["cost"]
+
+def calculate_user_coin(user_coins):
+    calculated_coin = {}
+    for coin in user_coins:
+        calculated_coin[coin] = round(coins[coin] * user_coins[coin],2)
+        
+    total_amount = 0
+    for coin in calculated_coin:
+        total_amount += calculated_coin[coin]
+        
+    return total_amount
+        
+def insert_coins(coffee_name):
+    is_enough_money = "yes"
+    user_input_coin = {}
+    msg = ""
+    
+    for coin in coins:
+        try:
+            user_input_coin.update({coin: int(input(f"{coin}: $"))}) 
+        except:
+            
+            total_amount = calculate_user_coin(user_input_coin)
+            
+            print(f"\nCoins Refunded: ${total_amount}")
+            msg = "Invalid Coin entry"
+
+            is_enough_money = "no"
+    
+    coffee_price = get_coffee_price(coffee_name)
+    total_amount = calculate_user_coin(user_input_coin)
+    
+    if(total_amount != coffee_price):
+        is_enough_money = "no"
+        msg = "Sorry that's not enough money."
+        print(f"\nMoney Refunded: ${total_amount}")
+
+    return is_enough_money, msg
+
+def is_enough_resource_available(coffee_resources, coffee_name, machine = None):
     is_enough_resource = "yes"
     ingredient = ""
     for resource in resources:
-        if(resource in coffee_resources):
             if(resources[resource] - coffee_resources[resource] < 0):
                 is_enough_resource = "no"
                 ingredient = resource
-                print()
-            else:  
-                resources.update({resource :resources[resource] - coffee_resources[resource]})
 
-    return is_enough_resource, ingredient
+    return is_enough_resource, ingredient  
     
-    
-def calculate_coffee_resources(coffee_name):
+def use_resource(coffee_name):
+    for resource in MENU[coffee_name]["ingredients"]:
+        resources.update({resource: resources[resource] - MENU[coffee_name]["ingredients"][resource]})
+
+def calculate_coffee_resources(coffee_name, machine = None):
     coffee_resources = MENU[coffee_name]["ingredients"]
     available, unavailable_ingredient = is_resources_available(coffee_resources, coffee_name)
     if(available == "yes"):
-        is_enough_resource, ingredient = is_enough_resource_available(coffee_resources, coffee_name)
-        if(is_enough_resource == "yes"):
-            print(f"Enjoy sir, Here is you {coffee_name}")    
-        else:
+        is_enough_resource, ingredient = is_enough_resource_available(coffee_resources, coffee_name, machine)
+        if(is_enough_resource != "yes"):
             print(f"Sorry there is not enough {ingredient}")
+        else:
+             # here are you pending calulating resouces 
+            is_enough_money, msg = insert_coins(coffee_name)
+            if(is_enough_money == "yes"):
+                use_resource(coffee_name)
+                print(f"Enjoy sir, Here is your {coffee_name}")    
+                restart_machine = machine()
+            else:
+                print(msg)
+                restart_machine = machine()
+                    
     else:
         print(f"{coffee_name.capitalize()} requires {unavailable_ingredient}, which we don’t have right now.")
         
-def check_coffee(user_input):
+def check_coffee(user_input, machine = None):
     if user_input in MENU:
-        calculate_coffee_resources(coffee_name=user_input)
+        calculate_coffee_resources(user_input, machine)
     else:
         return "Invalid coffee name"
 
@@ -106,6 +161,6 @@ def coffeeMachine():
             report(machine = coffeeMachine)
             on = False
         else:
-            check_coffee(user_input)
+            check_coffee(user_input, machine = coffeeMachine)
 
 coffeeMachine()
